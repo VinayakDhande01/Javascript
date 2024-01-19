eade62c7399a108f83f3b2e4e64138d327625183
Dhande Vinayak Shamrao <133940711+VinayakDhande01@users.noreply.github.com>
2024-01-19 06:59:50 +0000

    Datatypes in js

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+let name="Vinayak"
+let age=22
+let is18=true
+console.log(typeof age)
+
+
+// Primitive datatypes 
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
+//non primitive data types
+// object
