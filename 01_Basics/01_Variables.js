5f0047fad2aaf0bd2426b5bfa03419c8d1212131
Dhande Vinayak Shamrao <133940711+VinayakDhande01@users.noreply.github.com>
2024-01-19 06:32:54 +0000

    Varibles declaration

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId=12323 // further values cant changed
+let accountName="hbhsbbduc"
+let accountEmail="vina@gmail.com"
+var accountPassword="3344344"
+// never use var
+accountCity="Pune"
+
+accountName="736377df"
+accountEmail="vin@hd.com"
+accountPassword="gduhdudh"
+accountCity ="Mumbai"
+let accountState
+// console.log(accountName)
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity,accountState])
